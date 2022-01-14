13dce2eb5a2623575ba76c632a3d517f9464e79f
conseicka <>
2022-01-13 20:01:32 -0600

    using cookies and respondig with a redirection

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    #getting user ip
+    user_ip = request.remote_addr
+
+    #redirecting from index to hello
+    response = make_response(redirect("/hello"))
+    #seting user_ip as cookie variable
+    response.set_cookie("user_ip", user_ip)
+
+    return response
+
+@app.route("/hello")
 def hello():
-    return "Hello World Flask"
+    user_ip = request.cookies.get("user_ip")
+
+    return f"Hello World Flask {user_ip}"
