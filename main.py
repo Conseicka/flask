44f511da20d247dbe3f40dd15cc7cd674c472cdb
conseicka <>
2022-01-14 21:33:23 -0600

    static files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, make_response, redirect, render_template
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__,template_folder='templates', static_folder='./static')
 
 todos = ["Comprar cafe", "Enviar solicitud de compra", "Entregar un video"]
 
