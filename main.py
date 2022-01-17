40d033b226e227cb276068dcfec7a7e67d1e1d78
conseicka <>
2022-01-16 22:19:22 -0600

    update task

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from flask_bootstrap import Bootstrap
 from flask_login import login_required
 from flask_login import current_user
 from app import create_app
-from app.forms import TodoForm, DeleteTodoForm
-from app.firestore_service import get_users, get_todos, put_todo, delete_todo
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+from app.firestore_service import update_todo, get_todos, put_todo, delete_todo
 
 
 app= create_app()
@@ -52,13 +52,15 @@ def hello():
     username = current_user.id
     todo_form = TodoForm()
     delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
 
     context = {
         "user_ip": user_ip,
         "todos": get_todos(user_id=username),
         "username": username,
         "todo_form": todo_form,
-        "delete_form": delete_form
+        "delete_form": delete_form,
+        "update_form": update_form,
     }
     if todo_form.validate_on_submit():
         put_todo(user_id=username, description=todo_form.description.data)
@@ -76,3 +78,10 @@ def delete(todo_id):
     delete_todo(user_id=user_id, todo_id=todo_id)
 
     return redirect(url_for('hello'))
+
+
+@app.route("/todos/update/<todo_id>/<int:done>", methods=["POST"])
+def update(todo_id, done):
+    user_id = current_user.id
+    update_todo(user_id=user_id, todo_id=todo_id, done=done)
+    return redirect(url_for("hello"))
