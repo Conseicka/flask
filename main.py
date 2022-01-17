7e86126aef5593927e48aceaa56a45474103081e
conseicka <>
2022-01-16 21:50:33 -0600

    deleting tasks

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from flask import url_for
 from flask import redirect
 from flask import make_response
 from flask import render_template
+#from flask.app.firestore_service import delete_todo
 from flask_bootstrap import Bootstrap
 from flask_login import login_required
 from flask_login import current_user
 from app import create_app
-from app.forms import TodoForm
-from app.firestore_service import get_users, get_todos, put_todo
+from app.forms import TodoForm, DeleteTodoForm
+from app.firestore_service import get_users, get_todos, put_todo, delete_todo
 
 
 app= create_app()
@@ -50,12 +51,14 @@ def hello():
     user_ip = session.get("user_ip")
     username = current_user.id
     todo_form = TodoForm()
+    delete_form = DeleteTodoForm()
 
     context = {
         "user_ip": user_ip,
         "todos": get_todos(user_id=username),
         "username": username,
         "todo_form": todo_form,
+        "delete_form": delete_form
     }
     if todo_form.validate_on_submit():
         put_todo(user_id=username, description=todo_form.description.data)
@@ -67,3 +70,9 @@ def hello():
     #the "**" is used to expand all the content of context and make easier to handled it in jinja2
     return render_template("hello.html", **context)
     
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id=user_id, todo_id=todo_id)
+
+    return redirect(url_for('hello'))
